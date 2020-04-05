fa344a229552099eb7478287feb9a9371189404a
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-04-05 19:47:19 -0400

    created applicationDbContext class

---
+++ COMP212_Xamarin_SQLite/COMP212_Xamarin_SQLite.Android/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using COMP212_Xamarin_SQLite.Droid.Model;
+using SQLite;
+
+namespace COMP212_Xamarin_SQLite.Droid.Data
+{
+    public  class ApplicationDbContext
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public ApplicationDbContext(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Movie>().Wait();
+
+        }
+
+
+    }
+}
