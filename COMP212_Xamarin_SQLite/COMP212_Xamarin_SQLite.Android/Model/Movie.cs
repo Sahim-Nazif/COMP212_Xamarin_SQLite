9aa57741c8ac2b7db0bb11f4f0728c1e27654b2a
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-04-05 19:16:56 -0400

    added Model-Class with properties

---
+++ COMP212_Xamarin_SQLite/COMP212_Xamarin_SQLite.Android/Model/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace COMP212_Xamarin_SQLite.Droid.Model
+{
+   public class Movie
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public DateTime Release_Date { get; set; }
+
+    }
+}
